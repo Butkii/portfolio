50befa55e4dac5fae3f3701440ed69522538d327
Bhakti Ghaghda <72260135+Butkii@users.noreply.github.com>
2023-08-11 12:11:55 +0400

    Setup TailwindCSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import './App.css';
+// // import Header from './components/Header';
+// // import About from './components/About';
+// // import Projects from './components/Projects';
+// // import Resume from './components/Resume';
+// // import Contact from './components/Contact';
+// // import SocialLinks from './components/SocialLinks';
+
+// function App() {
+//   return (
+//     <div className="bg-background min-h-screen font-open-sans">
+//       <div className="text-3xl font-bold underline">Hello</div>
+//       {/* <Header />
+//       <div className="container mx-auto p-4">
+//         <About />
+//         <Projects />
+//         <Resume />
+//         <Contact />
+//         <SocialLinks />
+//       </div> */}
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from 'react';
+import './App.css'; // Make sure you're importing the correct CSS file
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-background min-h-screen font-open-sans">
+      <div className="text-7xl">Helloo</div>
     </div>
   );
 }
